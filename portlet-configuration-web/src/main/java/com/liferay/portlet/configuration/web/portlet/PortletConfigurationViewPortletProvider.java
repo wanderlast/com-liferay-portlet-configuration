4ed12e6a110e7c78cb79dbd290fc0280c8ce46cb
Brian Chan <brian.chan@liferay.com>
2016-03-08 11:34:44 -0800

    LPS-64031 rename modules/apps/web-experience-management/ to modules/apps/web-experience

---
+++ portlet-configuration-web/src/main/java/com/liferay/portlet/configuration/web/portlet/PortletConfigurationViewPortletProvider.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portlet.configuration.web.portlet;
+
+import com.liferay.portal.kernel.portlet.BasePortletProvider;
+import com.liferay.portal.kernel.portlet.ViewPortletProvider;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portlet.configuration.kernel.util.PortletConfigurationApplicationType;
+import com.liferay.portlet.configuration.web.constants.PortletConfigurationPortletKeys;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Juergen Kappler
+ */
+@Component(
+	immediate = true,
+	property = {
+		"model.class.name=" + PortletConfigurationApplicationType.PortletConfiguration.CLASS_NAME
+	},
+	service = ViewPortletProvider.class
+)
+public class PortletConfigurationViewPortletProvider
+	extends BasePortletProvider implements ViewPortletProvider {
+
+	@Override
+	public String getPortletName() {
+		return PortletConfigurationPortletKeys.PORTLET_CONFIGURATION;
+	}
+
+	@Override
+	protected long getPlid(ThemeDisplay themeDisplay) {
+		return themeDisplay.getPlid();
+	}
+
+}
